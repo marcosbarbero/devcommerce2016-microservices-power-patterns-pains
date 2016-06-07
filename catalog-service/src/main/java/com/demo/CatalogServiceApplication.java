940b9988ae2739079926926dd3feed70a75a8cc6
Marcos Barbero <marcos.hgb@gmail.com>
2016-06-06 22:44:37 -0300

    Fix: fallback

--- catalog-service/src/main/java/com/demo/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/demo/CatalogServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+
+@Data
+@ConfigurationProperties(prefix = "catalog")
+class CatalogProperties {
+    private List<String> products = new ArrayList<>();
+}
+
+@Service
+class ThrowableService {
+
+    @HystrixCommand(fallbackMethod = "fallbackThrowUpMethod")
+    public Collection<String> throwUP() {
+        throw new RuntimeException("throwUP");
+    }
+
+    private Collection<String> fallbackThrowUpMethod() {
+        return Arrays.asList("it,works".split(","));
+    }
+}
 
 /**
  * Dummie catalog service.
@@ -31,30 +51,20 @@ public class CatalogServiceApplication {
     }
 
     @Autowired
-    CatalogProperties catalog;
+    private CatalogProperties catalog;
+
+    @Autowired
+    private ThrowableService service;
 
     @RequestMapping("/products")
     public Collection<String> products() {
-        return catalog.products();
+        return catalog.getProducts();
     }
 
     @RequestMapping("/throw")
-    @HystrixCommand(fallbackMethod = "fallbackThrowUpMethod")
     public Collection<String> throwUP() {
-        throw new RuntimeException("throwUP");
+        return this.service.throwUP();
     }
 
-    private Collection<String> fallbackThrowUpMethod(RuntimeException e) {
-        return Arrays.asList("it,works".split(","));
-    }
-}
 
-@Data
-@ConfigurationProperties(prefix = "catalog")
-class CatalogProperties {
-    List<String> products = new ArrayList<>();
-
-    public List<String> products() {
-        return this.products;
-    }
 }
